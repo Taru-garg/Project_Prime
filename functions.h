6c4195a95be621c36339bf94a4b55f8fecb49475
Taru <tarugarg2000@gmail.com>
2019-10-14 20:09:10 +0530

    files
    Co-Authored-By: Taru Garg <tarugarg2000@gmail.com>

--- functions.h
+++ functions.h
@@ -1,16 +1,32 @@
 //functions.h
+/*This function is used to print anywhere the answer where 1 = maybe a prime; 0 = not a prime and 2 = definetly a prime number*/
+void answer(int prediction)
+{
+	if(prediction == 0)
+	{
+		printf("Not a prime");
+	}
+	else if (prediction == 1)
+	{
+		printf("Maybe or may not be a prime");
+	}
+	else if (prediction == 2)
+	{
+		printf("Definetly a prime");
+	}
+}
 /*The function basic accepts a single parameter and checks the most basic condition to tell wether or not a number is prime.The basic condition used is that any
 prime number will always be of the form 6n-1 or 6n+1 though this is a necessary condition but not sufficient*/
 int basic(long long int num)
 {
 	if(((num+1)%6 == 0) || ((num-1)%6 == 0) || num == 2 || num == 3)
 	{
-		printf("This number may or may not be a prime");
+		answer(1);
 		return 1;
 	}
 	else
 	{
-		printf("This number is definetly not a prime");
+		answer(0);
 		return 0;
 	}
 	
