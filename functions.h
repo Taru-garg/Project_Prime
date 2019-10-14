df20f0bcb6e0870d4a36ee966c1784a55627ac00
Taru <tarugarg2000@gmail.com>
2019-10-14 19:31:13 +0530

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,4 +1,6 @@
 //functions.h
+/*The function basic accepts a single parameter and checks the most basic condition to tell wether or not a number is prime.The basic condition used is that any
+prime number will always be of the form 6n-1 or 6n+1 though this is a necessary condition but not sufficient*/
 int basic(long long int num)
 {
 	if(((num+1)%6 == 0) || ((num-1)%6 == 0) || num == 2 || num == 3)
