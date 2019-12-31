11aecdbfe691d2d701bf6cc2642934d4c9d22085
Taru Garg <tarugarg2000@gmail.com>
2019-12-31 23:15:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
 /*Project_Prime*/
-/* The following code has been developed as a tribute to  all the people who dedicated their life towarrds the study of prime numbers. Prime numbers have always been a fascinating topic
-yet very controversial. Here we have considered some basic results and then moved on to more sophisticated primlality tests. After making any changes please sure that the basic nature of the code is preserved and that is the speed with which it can check the numbers for their primality.
-Various checkpoints have been incorporated within the code, so if in case you make a change that instead of improvement of code deteriorated it you can just confirm and understand the problem
-with help of checkpoints.*/
 
 #include <iostream>
 #include <cmath>
