8c851d583943465e6acf123590096eb3aa3de5b1
Taru Garg <tarugarg2000@gmail.com>
2019-12-31 23:15:52 +0530

    Update algorithms.h

--- algorithms.h
+++ algorithms.h
@@ -1,9 +1,4 @@
 //algorithms.h
-/*The simplest probabilistic primality test is the Fermat primality test (actually a compositeness test). It works as follows:
-Given an integer n, choose some integer a coprime to n and calculate a^(n - 1) modulo n. If the result is different from 1, then n is composite. If it is 1, then n may be prime.
-If a^(n-1) (modulo n) is 1 but n is not prime, then n is called a pseudoprime to base a. In practice, we observe that, if a^(n-1) (modulo n) is 1, then n is usually prime. But here is a counterexample: if n = 341 and a = 2, then
-even though 341 = 11·31 is composite. In fact, 341 is the smallest pseudoprime base 2.There are only 21853 pseudoprimes base 2 that are less than 2.5×10^10. This means that, for n up to 2.5×1010, if 2n-1 (modulo n) equals 1, then n is prime, unless n is one of these 21853 pseudoprimes.
-Some composite numbers (Carmichael numbers) have the property that an - 1 is 1 (modulo n) for every a that is coprime to n. The smallest example is n = 561 = 3·11·17, for which a560 is 1 (modulo 561) for all a coprime to 561. */
 int fermat_method (unsigned long long int a)
 {
 	unsigned long long int k;
